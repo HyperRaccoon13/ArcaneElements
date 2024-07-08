00d5c9fd606f4be79292b65ef7b89fe17e7a55c8
HyperRaccoon13 <daraccoon133@gmail.com>
2024-07-08 09:04:57 +0100

    1.21 here we goooooo

---
+++ src/client/java/com/github/hyperraccoon13/ArcaneElementsClient.java
@@ -0,0 +1,10 @@
+package com.github.hyperraccoon13;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaneElementsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/github/hyperraccoon13/ArcaneElementsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.hyperraccoon13;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArcaneElementsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
